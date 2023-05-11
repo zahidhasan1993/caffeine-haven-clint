a97d685fa801b27abea102bbac949067c552f384
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-12 05:40:31 +0600

    adding delete function from database

--- src/components/extra/CoffeeCard.jsx
+++ src/components/extra/CoffeeCard.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import { HiEye, HiPencil, HiTrash } from "react-icons/hi";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ data }) => {
-  const { photo, name, chef, category } = data;
-  console.log(data);
+  const {_id, photo, name, chef, category } = data;
+  // console.log(data);
+
+  const handleDelete = (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        fetch(`http://localhost:5000/coffee/${id}`,{
+          method : "DELETE",
+        })
+        .then(res => res.json())
+        .then(data => {
+          // console.log(data);
+          Swal.fire(
+          'Deleted!',
+          'Your coffee has been deleted.',
+          'success'
+        )
+        })
+      }
+    })
+  }
 
   return (
     <div>
@@ -31,7 +61,7 @@ const CoffeeCard = ({ data }) => {
           <br />
           <button className="btn btn-black"><HiPencil className="text-2xl"></HiPencil></button>
           <br /> <br />
-          <button className="btn bg-red-800 hover:bg-red-600"><HiTrash className="text-2xl"></HiTrash></button>
+          <button onClick={() => handleDelete(_id)} className="btn bg-red-800 hover:bg-red-600"><HiTrash className="text-2xl"></HiTrash></button>
         </div>
       </div>
     </div>
